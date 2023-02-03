b744452dfa6867e1a10c7442e33fcba4335d9a5b
nenaleeskus <nenaleeskus@gmail.com>
2023-02-03 21:05:39 +0300

    dj hw

---
+++ news/models.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.models import User
+from django.db import models
+from .res import *
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = Post.objects.filter(author=self).aggregate(models.Sum('rating'))
+        author_comment_rating = Comment.objects.filter(user=self.user).aggregate(models.Sum('rating'))
+        all_comment_rating = Comment.objects.filter(post__in=Post.objects.filter(author=self)).aggregate(
+            models.Sum('rating'))
+        self.rating = post_rating['rating__sum'] * 3 + \
+                      author_comment_rating['rating__sum'] + \
+                      all_comment_rating['rating__sum']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=sport, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type_choice = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=244)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def preview(self):
+        return self.text[:124] + '...'
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=244)
+    date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
---
+++ news/res.py
@@ -0,0 +1,20 @@
+sport = 'SP'
+politics = 'PL'
+economics = 'EC'
+education = 'ED'
+
+CATEGORY_CHOICES = [
+    (sport, 'Sport'),
+    (politics, 'Politics'),
+    (economics, 'Economics'),
+    (education, 'Education')
+]
+
+
+news = 'NW'
+article = 'AR'
+
+TYPE_CHOICES = [
+    (news, 'News'),
+    (article, 'Article')
+]
